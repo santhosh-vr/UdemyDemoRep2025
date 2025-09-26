9f902e185ecabd9fae256a71ac290d59ff318397
Santhosh <vrsan28@gmail.com>
2025-09-26 22:53:45 +0530

    Added from Lap

--- src/ListConcept/gitDemo.java
+++ src/ListConcept/gitDemo.java
@@ -5,6 +5,8 @@ public class gitDemo {
 	public static void main(String[] args) {
 		System.out.println("Added for Git clone");
 		System.out.println("-------------------");
+		
+		System.out.println("Added from Lap");
 
 	}
 
